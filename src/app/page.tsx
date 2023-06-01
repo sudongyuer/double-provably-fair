ceffa90b9867e9b23f6db381f1931eb751d5c8b4
sudongyuer <976499226@qq.com>
2023-06-01 11:37:10 +0800

    chore: update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,100 @@
-import Image from 'next/image'
+"use client"
+import {useState} from "react";
+import crypto from "crypto";
+
+const TILES = [
+  { number: 0, color: "white" },
+  { number: 11, color: "black" },
+  { number: 5, color: "red" },
+  { number: 10, color: "black" },
+  { number: 6, color: "red" },
+  { number: 9, color: "black" },
+  { number: 7, color: "red" },
+  { number: 8, color: "black" },
+  { number: 1, color: "red" },
+  { number: 14, color: "black" },
+  { number: 2, color: "red" },
+  { number: 13, color: "black" },
+  { number: 3, color: "red" },
+  { number: 12, color: "black" },
+  { number: 4, color: "red" }
+];
 
 export default function Home() {
+  const [server_seed, setServer_seed] = useState("");
+  const [amount, setAmount] = useState(10);
+  const chain = [server_seed];
+
+  for (let i = 0; i < amount; i++) {
+    chain.push(
+        crypto
+            .createHash("sha256")
+            .update(chain[chain.length - 1])
+            .digest("hex")
+    );
+  }
+
+  // the hash of bitcoin block 570120 (https://medium.com/@blazedev/blaze-com-double-seeding-event-d3290ef13454)
+  const clientSeed =
+      "00000000000000000004172a4be28d9cdf7e5e36836f1fc6a106ae73266bf47a";
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="App">
+        <h3>Enter the server seed of your game</h3>
+        <input
+          value={server_seed}
+          onChange={(e) => setServer_seed(e.target.value)}
         />
+        <br />
+        <br />
+        <h3>Enter the # of games to view before this one</h3>
+        <input
+          value={amount}
+          onChange={(e) =>setAmount(Number(e.target.value))}
+        />
+        <hr />
+        <h1>Double rolls:</h1>
+        {!server_seed || server_seed.length !== 64 ? (
+          <h3 style={{ color: "red" }}>
+            Please enter a server seed to view this table
+          </h3>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>Roll</th>
+                <th>Seed</th>
+                <th>Hash (hmac with client seed)</th>
+              </tr>
+            </thead>
+            <tbody>
+              {chain.map((seed, index) => {
+                const hash = crypto
+                  .createHmac("sha256", seed)
+                  .update(clientSeed)
+                  .digest("hex");
+                console.log(hash);
+                // roulette number from 0-15
+                const result = parseInt(hash.slice(0, 13), 16) % 15;
+                console.log(result);
+
+                const tile = TILES.find((t) => t.number === result);
+
+                return (
+                  <tr key={index}>
+                    <td style={{ color: tile?.color }}>
+                      {tile!.color.slice(0, 1).toUpperCase() +
+                        tile!.color.slice(1)}{" "}
+                      {result}
+                    </td>
+                    <td>{seed}</td>
+                    <td>{hash}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    )
 }
